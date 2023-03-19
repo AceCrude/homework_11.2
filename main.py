79c8c6bc5b849c32038cf51b1486528f5e5bcea2
AceCrude <artems.9797@gmail.com>
2023-03-20 00:07:05 +0300

    commit sum

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from funcs import get_sum
+from funcs import get_sum, get_sub
 
 sum_1 = 3
 sum_2 = 5
@@ -6,3 +6,9 @@ sum = get_sum(sum_1,sum_2)
 
 
 print(sum)
+
+sub_1 = 5
+sub_2 = 3
+sub = get_sub(sub_1, sub_2)
+
+print(sub)
