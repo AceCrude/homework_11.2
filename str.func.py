492646f927cdf3111b66a4b9876f671379da4478
AceCrude <artems.9797@gmail.com>
2023-03-22 20:22:21 +0300

    added str

---
+++ str.func.py
@@ -0,0 +1,20 @@
+def caps_input(str):
+    """docstring
+    """
+    return str.upper()
+
+
+caps = caps_input("skypro")
+
+print(caps)
+
+
+def caps_input(str):
+    """docstring
+    """
+    return str.title()
+
+
+caps = caps_input("skypro and skyeng")
+
+print(caps)
