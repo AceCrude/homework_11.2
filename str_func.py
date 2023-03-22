56ae15c6306fbbdf847d84c50bf3815431e2a827
AceCrude <artems.9797@gmail.com>
2023-03-22 23:35:12 +0300

    new_func_2

--- str_func.py
+++ str_func.py
@@ -7,3 +7,14 @@ def caps_input(str):
 caps = caps_input("skypro")
 
 print(caps)
+
+
+def caps_input(str):
+    """docstring
+    """
+    return str.title()
+
+
+caps = caps_input("skypro and skyeng")
+
+print(caps)
